78c3a158a223eff601950d902382338f4b704944
ysdinesh31 <30742447+ysdinesh31@users.noreply.github.com>
2021-12-09 01:43:05 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# PoseEstimationTracking
-EEE511 Project
+# Human Pose Estimation and Tracking
+**This research/code is cited from: (Zhe Cao, Gines Hidalgo, Tomas Simon, Shih-En Wei, Yaser Sheikh, OpenPose: Realtime Multi-Person 2D Pose Estimation using Part Affinity Fields) & (Steven Chen, Richard R. Yang, Pose Trainer: Correcting Exercise Posture using Pose Estimation)**
+
+## Introduction
+In this project uses the concept of OpenPose to detect and track human body by generating keypoints on body joints and then connecting them forming a skeleton.This project was later extended to track a person performing the bicep curls workout and count the repitions only when the form of the exercise is proper. This projects uses the model developed by CMU <a href = 'https://github.com/CMU-Perceptual-Computing-Lab/openpose'>Carnegie Mellon University</a>.
+
+## Directory Structure
+
+## Python files
+all the files are obtained from https://github.com/akhilvasvani/StackGAN-v2/tree/master/code except generate_images.py which is our built in code.
+There are many files to list but here are the imported ones
+  - **bicepcurl.py** main file that tracks and counts the bicep curl
+  - **OpenPose_Notebook.py** performs basic pose estimation tasks
+## How to run code
+1) First download the model files from https://github.com/CMU-Perceptual-Computing-Lab/openpose or use the following command to clone the entire repository: gh repo clone CMU-Perceptual-Computing-Lab/openpose
+2) Save all the files as mentioned in the directory structure and then run the code using the following commands
+
+## Dependencies for executing
+
+python 3.6+
+Visual Studio Code or any other editor
+OpenCV
+Mediapipe 
+NumPy
+
+
+
--- bicepcurl.py
+++ bicepcurl.py
@@ -1,14 +1,16 @@
+# Importing required libraries 
 import math
 import cv2
 import mediapipe as mp
 import numpy as np
+# Function used to draw lines to connect keypoints and generate a skeleton
 mp_drawing = mp.solutions.drawing_utils
-# mp_drawing_styles = mp.solutions.drawing_styles
+# Used to estimate keypoints
 mp_pose = mp.solutions.pose
-
+# Initializing variables to store values of count and direction of hand motion from camera
 count = 0
 dir = 0
-
+# Function to count the Bicep Curls
 def  countcurls(image,keypoints):
     global count , dir
     if len(keypoints) != 0:
@@ -17,12 +19,11 @@ def  countcurls(image,keypoints):
         h1 = int((1100/1280)*h)
         w2 = int((650/720)*w)
         h2 = int((1175/1280)*h)
+        # Finding angle of using keypoints 12,14 and 16 for right arm
         angle = findAngle(image, 12, 14, 16,keypoints)
-        # angle = findAngle(image, 11, 13, 15,keypoints)
+        # Setting the max and min angle threshold for counting the curls
         per = np.interp(angle, (60, 150), (0, 100))
-        bar = np.interp(angle, (60, 150), (w2, 100))
-        print(angle, per,bar)
- 
+        print(angle, per)
         color = (255, 0, 255)
         if per == 0:
             color = (0, 255, 0)
@@ -35,36 +36,27 @@ def  countcurls(image,keypoints):
                 count += 0.5
                 dir = 0
         print(count,dir)
-
- 
-        # cv2.rectangle(image, (h1, w1), (h2, w2), color, 3)
-        # cv2.rectangle(image, (h1, int(bar)), (h2, w2), color, cv2.FILLED)
-        # cv2.putText(image, f'{int(per)} %', (h1-70, 50), cv2.FONT_HERSHEY_PLAIN, 4,
-        #             color, 4)
- 
-
-
         w1 = int((450/720)*w)
         h1 = int((0/1280)*h)
         w2 = int((720/720)*w)
-        h2 = int((500/1280)*h)
-        
+        h2 = int((500/1280)*h)        
         cv2.rectangle(image, (h1, w1), (h2, w2), (255, 255, 0), cv2.FILLED)
         cv2.putText(image, str(int(count)), (h1+80, w2), cv2.FONT_HERSHEY_PLAIN, 15,
                     (255, 0, 0), 10)
         
         
-        
-
+ # Function to calculate the angle. # keypoints are used as each arm has three keypoints as labelled by the model
 def findAngle(image, p1, p2, p3, keypoints,draw=True):
     x1, y1 = keypoints[p1][1:]
     x2, y2 = keypoints[p2][1:]
     x3, y3 = keypoints[p3][1:]
+# Calculating angle using slope between the three points
     angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
                             math.atan2(y1 - y2, x1 - x2))
     if angle < 0:
         angle += 360
     if draw:
+# Following lines serve the purpose of highlighting the detected keypoints 
         cv2.line(image, (x1, y1), (x2, y2), (255, 255, 255), 3)
         cv2.line(image, (x3, y3), (x2, y2), (255, 255, 255), 3)
         cv2.circle(image, (x1, y1), 10, (0, 0, 255), cv2.FILLED)
@@ -77,7 +69,7 @@ def findAngle(image, p1, p2, p3, keypoints,draw=True):
                     cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
     return angle
 
-
+# Function to find the position of keypoints in the frame
 def findPosition(image,results):
     keypoints = []
     if results.pose_landmarks:
@@ -85,21 +77,15 @@ def findPosition(image,results):
             h, w, c = image.shape
             cx, cy = int(lm.x * w), int(lm.y * h)
             keypoints.append([id, cx, cy])
-    # mp_drawing.draw_landmarks(
-    # image,
-    # results.pose_landmarks,
-    # mp_pose.POSE_CONNECTIONS)
     return keypoints
-
+# Taking input from the camera
 cap = cv2.VideoCapture(0)
-# cap = cv2.VideoCapture("/home/kesava/Desktop/curl.mp4")
 frame_counter = 0
 with mp_pose.Pose(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as pose:
     while cap.isOpened():
         success, image = cap.read()
-        # image = cv2.resize(image, (1280, 720))
         if not success:
             print("Ignoring empty camera frame.")
             break
